6935758bd4790e02d9726a160085c947d5d903d3
Nicholas Muriithi <nickstech707@gmail.com>
2024-07-07 19:11:15 +0300

    Uses the session object to authenticate a user.

--- server/app.py
+++ server/app.py
@@ -21,14 +21,12 @@ api = Api(app)
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class IndexArticle(Resource):
-    
+
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
@@ -40,18 +38,51 @@ class ShowArticle(Resource):
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
+            if not article:
+                return {'message': '404: Article not found'}, 404
             article_json = jsonify(article.to_dict())
-
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        if not user:
+            return {'message': '401: Unauthorized'}, 401
+
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            if not user:
+                return {}, 401
+            return user.to_dict(), 200
+        
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
